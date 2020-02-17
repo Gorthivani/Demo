5439f8240ab5a3e0ba2ca3633a3ab3498776b0c3
BVRIT <BVRIT@ECE-LAB-203>
2020-02-17 14:07:07 +0530

    project

---
+++ fact/src/capgemini/Factorial.java
@@ -0,0 +1,26 @@
+package capgemini;
+
+import java.util.Scanner;
+
+public class Factorial {
+	{
+      int i,fact;
+      for(int i=0; i<=n; i++)
+    	  fact=fact*i;
+	} 
+
+	
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in); 
+		System.out.println("Enter a number");
+		 
+	 
+		   
+	   
+		
+		
+		
+	}
+
+}
---
+++ fact/src/capgemini/polindrome.java
@@ -0,0 +1,23 @@
+package capgemini;
+
+import java.util.Scanner;
+
+public class polindrome {
+	
+	private static String s1;
+
+	public static void main(String[] args) {
+		String sr,rev =" ";
+		Scanner scan  = new Scanner(System.in); 
+	    System.out.println("Enter string");
+	    sr=scan.nextLine();
+	    int length=sr.length();
+		
+     	    
+ StringBuffer sb =new StringBuffer(s1);
+ String str1= scan.nextLine();
+ 
+ 
+	}
+
+}
